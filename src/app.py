59ba54815175bb861809ac1bbf3a344b75a80128
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-10-21 03:09:45 +0000

    finalized GET, POST and DELETE family members, all tests passed

--- src/app.py
+++ src/app.py
@@ -30,13 +30,41 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
+
+    return jsonify(members), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    return jsonify(member), 200
+    # this is how you can use the Family datastructure by calling its methods
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
     response_body = {
-        "hello": "world",
-        "family": members
+        "done": True
     }
-
-
+    print(member)
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = (request.get_json())
+    member = {
+        "first_name": body["first_name"],
+        "last_name": "Jackson",
+        "id": body["id"],
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
+    }
+    # this is how you can use the Family datastructure by calling its methods
+    new_member = jackson_family.add_member(member)
+    response_body = {
+        "msg": "A new family member was added"
+    }
+    print(response_body)
+    return jsonify(new_member), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
